f4ecfc72b62bcbebeaf1ae5cf55d360ba7b038f9
Idowu Joseph <idowujo672@gmail.com>
2026-06-19 16:37:00 +0100

    Fix all route file casing to match imports

--- front-end/src/components/ProtectedRoute.jsx
+++ front-end/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, useLocation } from "react-router-dom";
-import useAuthStore from "../stores/useAuthStore";
+import useAuthStore from "../stores/useauthstore";
 
 export default function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuthStore();
