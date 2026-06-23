dd8ae196bfc6b90c33fd94592896ec46dfac8a7a
Idowu Joseph <idowujo672@gmail.com>
2026-06-23 11:54:55 +0100

    made some changes

--- admin/src/lib/api.js
+++ admin/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: "https://trade-x-4lcn.onrender.com" || "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
 
